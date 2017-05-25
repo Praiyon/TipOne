f86f9390af20ca421425950f78158a4f41ca0c95
praiyon <praiyon@gmail.com>
2017-05-25 18:00:40 -0400

    first commit

---
+++ app/src/main/java/com/example/praiyon/tipone/Help.java
@@ -0,0 +1,29 @@
+package com.example.praiyon.tipone;
+
+/**
+ * Created by praiyon on 25/05/17.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Help extends Fragment {
+    public static Help newInstance() {
+        Help fragment = new Help();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.help, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/praiyon/tipone/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.praiyon.tipone;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+public class MainActivity extends AppCompatActivity {
+
+    //private TextView mTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.navigation_home:
+                                selectedFragment = Home.newInstance();
+
+
+                                break;
+                            case R.id.navigation_help:
+                                selectedFragment = Help.newInstance();
+
+                                break;
+
+                            case R.id.navigation_settings:
+                                selectedFragment = Home.newInstance();
+                                break;
+                        }
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.content, selectedFragment);
+                        transaction.commit();
+                        return true;
+                    }
+                });
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content, Home.newInstance());
+        transaction.commit();
+
+        //Manually displaying the first fragment - one time only
+
+
+        //Used to select an item programmatically
+        //bottomNavigationView.getMenu().getItem(2).setChecked(true);
+    }
+
+}
